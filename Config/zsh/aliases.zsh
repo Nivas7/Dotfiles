f6326ff45b755add685a7f86d1f5f7939c46f656
Nivas7 <nivasniyas24@gmail.com>
2025-01-20 13:25:20 +0530

    reconfigure zsh

---
+++ Config/zsh/aliases.zsh
@@ -0,0 +1,57 @@
+##
+## Aliases
+##
+
+alias run='pnpm run'
+alias c="clear"
+alias q="exit"
+alias cleanram="sudo sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches'"
+alias trim_all="sudo fstrim -va"
+alias mkgrub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+alias mtar='tar -zcvf' # mtar <archive_compress>
+alias utar='tar -zxvf' # utar <archive_decompress> <file_list>
+alias z='zip -r' # z <archive_compress> <file_list>
+alias uz='unzip' # uz <archive_decompress> -d <dir>
+alias sr='source ~/.config/zsh/env.zsh'
+alias ..="cd .."
+alias psg="ps aux | grep -v grep | grep -i -e VSZ -e" 
+alias mkdir="mkdir -p"
+alias fm='ranger'
+
+
+## Other Handy Aliases
+
+alias pacin="pacman -Slq | fzf -m --preview 'cat <(pacman -Si {1}) <(pacman -Fl {1} | awk \"{print \$2}\")' | xargs -ro sudo pacman -S"
+alias yayin="yay -Slq | fzf -m --preview 'cat <(yay -Si {1}) <(yay -Fl {1} | awk \"{print \$2}\")' | xargs -ro  yay -S"
+alias pacrem="pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' | xargs -ro sudo pacman -Rns"
+alias pac="pacman -Q | fzf"
+alias yaycom="yay -Gc"
+alias yaypd="yay -Qua"
+alias pacupd="pacman -Qu"
+alias yaycheck="yay -Gp"
+alias cleanpac='sudo pacman -Rns $(pacman -Qtdq); yay -c'
+alias installed="grep -i installed /var/log/pacman.log"
+
+alias ls="exa --color=auto --icons"
+alias l="ls -l"
+alias la="ls -a"
+alias lla="ls -la"
+alias lt="ls --tree"
+alias cat="bat --color always --plain"
+alias grep='grep --color=auto'
+alias mv='mv -v'
+alias cp='cp -vr'
+alias rm='rm -vr'
+
+## Git Stuffs
+
+alias commit="git add . && git commit -m"
+alias push="git push"
+alias git-rm="git ls-files --deleted -z | xargs -0 git rm"
+alias g=git
+alias ga='git add'
+alias gaa='git add --all'
+alias gav='git add --verbose'
+alias gb='git branch'
+alias gd='git diff'
+# vim:ft=zsh
