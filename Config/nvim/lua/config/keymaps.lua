67230926753a2b740450d68db96eeb037b47a4f2
Nivas7 <nivasniyas24@gmail.com>
2025-01-24 21:14:36 +0530

    minor changes

--- Config/nvim/lua/config/keymaps.lua
+++ Config/nvim/lua/config/keymaps.lua
@@ -1,41 +1,41 @@
 local opts = { noremap = true, silent = true }
 
--- Delete a word backwards
+-- delete a word backwards
 vim.keymap.set("n", "db", 'vb"_d')
 
--- Select all
-vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+-- select all
+vim.keymap.set("n", "<c-a>", "gg<s-v>g")
 
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Copy to system clipboard" })
-vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "copy to system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "paste from system clipboard" })
 
--- Better window management
+-- better window management
 
-vim.keymap.set("n", "hs", ":split<Return>", opts)
-vim.keymap.set("n", "vs", ":vsplit<Return>", opts)
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to below split" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to above split" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
-vim.keymap.set("n", "<C-Up>", "<cmd>resize -2<CR>", { desc = "Resize split up" })
-vim.keymap.set("n", "<C-Down>", "<cmd>resize +2<CR>", { desc = "Resize split down" })
-vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Resize split left" })
-vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Resize split right" })
+vim.keymap.set("n", "hs", ":split<return>", opts)
+vim.keymap.set("n", "vs", ":vsplit<return>", opts)
+vim.keymap.set("n", "<c-h>", "<c-w>h", { desc = "move to left split" })
+vim.keymap.set("n", "<c-j>", "<c-w>j", { desc = "move to below split" })
+vim.keymap.set("n", "<c-k>", "<c-w>k", { desc = "move to above split" })
+vim.keymap.set("n", "<c-l>", "<c-w>l", { desc = "move to right split" })
+vim.keymap.set("n", "<c-up>", "<cmd>resize -2<cr>", { desc = "resize split up" })
+vim.keymap.set("n", "<c-down>", "<cmd>resize +2<cr>", { desc = "resize split down" })
+vim.keymap.set("n", "<c-left>", "<cmd>vertical resize -2<cr>", { desc = "resize split left" })
+vim.keymap.set("n", "<c-right>", "<cmd>vertical resize +2<cr>", { desc = "resize split right" })
 
 -- save and quit
 
-vim.keymap.set("n", "<leader>Q", "<cmd>wq!<CR>", { desc = "Force save and  quit" })
+vim.keymap.set("n", "<leader>q", "<cmd>wq!<cr>", { desc = "force save and  quit" })
 
--- Buffers
+-- buffers
 
-vim.keymap.set("n", "<leader>q", "<cmd>q!<CR>", { desc = "Close buffer/window" })
-vim.keymap.set("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Go to next buffer" })
-vim.keymap.set("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Go to previous buffer" })
-vim.keymap.set("n", "]b", "<cmd>bnext<CR>", { desc = "Go to next buffer" })
-vim.keymap.set("n", "[b", "<cmd>bprevious<CR>", { desc = "Go to previous buffer" })
+vim.keymap.set("n", "<leader>q", "<cmd>q!<cr>", { desc = "close buffer/window" })
+vim.keymap.set("n", "<s-l>", "<cmd>bnext<cr>", { desc = "go to next buffer" })
+vim.keymap.set("n", "<s-h>", "<cmd>bprevious<cr>", { desc = "go to previous buffer" })
+vim.keymap.set("n", "]b", "<cmd>bnext<cr>", { desc = "go to next buffer" })
+vim.keymap.set("n", "[b", "<cmd>bprevious<cr>", { desc = "go to previous buffer" })
 
--- Visual vim.keymap.sets
-vim.keymap.set("v", "<leader>r", '"hy:%s/<C-r>h//g<left><left>') -- Replace all instances of highlighted words
-vim.keymap.set("v", "<C-s>", ":sort<CR>") -- Sort highlighted text in visual mode with Control+S
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv") -- Move current line up
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- Move current line down
+-- visual vim.keymap.sets
+vim.keymap.set("v", "<leader>r", '"hy:%s/<c-r>h//g<left><left>') -- replace all instances of highlighted words
+vim.keymap.set("v", "<c-s>", ":sort<cr>") -- sort highlighted text in visual mode with control+s
+vim.keymap.set("v", "k", ":m '>-2<cr>gv=gv") -- move current line up
+vim.keymap.set("v", "j", ":m '>+1<cr>gv=gv") -- move current line down
